4ee05c48f48913cd9a627762cc6bcc4795e49625
okkio <dominicfarrington@gmail.com>
2022-01-11 17:25:41 +1030

    feat: minimal repo of nuget package load failure in NET6.0

---
+++ src/System.Drawing.Common.Assembly.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.Loader;
+using System;
+
+Console.WriteLine(Environment.Version);
+
+try
+{
+	var assembly = AssemblyLoadContext.Default.LoadFromAssemblyName(new System.Reflection.AssemblyName("System.Drawing.Common, Version=6.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51"));
+
+	Console.WriteLine($"Loaded: {assembly.FullName}");
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.ToString());
+}
+
+Console.Read();
